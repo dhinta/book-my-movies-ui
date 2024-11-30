b18203a8ff6cced52cee8507c1de004f5e4c9883
Debasish <relaxed.dhinta@gmil.com>
2024-11-30 15:38:10 +0530

    header skeleton and dependencies updated

--- src/shared/header/components/header.tsx
+++ src/shared/header/components/header.tsx
@@ -1,4 +1,14 @@
 import logo from '@/assets/images/logo.png';
+import { Button } from '@/vendors/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/vendors/ui/select';
+
+import { Menu, Search } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 export function Header() {
@@ -7,6 +17,33 @@ export function Header() {
       <Link to={'/'} className="w-28">
         <img src={logo} alt="logo" />
       </Link>
+
+      <div className="relative ml-4 w-3/4">
+        <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
+        <input
+          type="search"
+          name="q"
+          placeholder="Search..."
+          className="w-full rounded-md border border-gray-300 py-2 pl-10 pr-4 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          aria-label="Search"
+          autoComplete="off"
+        />
+      </div>
+
+      <Select>
+        <SelectTrigger className="w-[150px]  h-10 ml-4">
+          <SelectValue placeholder="City" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Hyderabad</SelectItem>
+          <SelectItem value="dark">Kolkata</SelectItem>
+          <SelectItem value="system">Bengaluru</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <Button variant="ghost" size="icon">
+        <Menu />
+      </Button>
     </header>
   );
 }
